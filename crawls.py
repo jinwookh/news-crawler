a44d45bb5f74bef7f5a2dfca7cc72413e9e62329
jinwook <jin942002@naver.com>
2019-02-08 14:40:45 +0900

    add seoul(), hankook(), mk() at crawls.py

--- crawls.py
+++ crawls.py
@@ -7,7 +7,7 @@ import sql_handler
 
 
 ALREADY_DONE_QUOTE = "Crawling have already done. Let's skip it"
-RESPONSE_ERROR_QOUTE = "Error: response error"
+RESPONSE_ERROR_QUOTE = "Error: response error"
 DATE_NONE_QUOTE = "Error: cannot crawl date from site"
 SS_LIST_ZERO_QUOTE = "Error: number of ss_list_elements are zero"
 HEADLINE_ZERO_QUOTE = "Error: number of headline is 0"
@@ -15,15 +15,17 @@ SECTION_LIST_ZERO_QUOTE = "Error: number of section_list_element is 0"
 SECTION_TXT_ZERO_QUOTE = "Error: number of section_txt_element is 0"
 ARTICLE_ZERO_QUOTE = "Error: number of article is 0"
 PAPERLIST_ZERO_QUOTE = "Error: number of papaerlist_element is 0"
+A_ZERO_QUOTE = "Error: number of a_element is 0"
 
 NAVER_SEARCH_URL = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query="
 CHOSUN_URL = "http://srchdb1.chosun.com/pdf/i_service/index_new.jsp"
 DONGA_URL = "http://news.donga.com/Pdf"
 KHAN_URL = "http://paoin.khan.co.kr/service/Khan/Default.aspx"
 MUNHWA_URL ="http://www.paoin.com/service/Munhwa/Default.aspx"
-KMIB_URL = "http://www.paoin.com/service/Kukinews/Default.aspx" 
-
-
+KMIB_URL = "http://www.paoin.com/service/Kukinews/Default.aspx"
+SEOUL_URL = "http://www.paoin.com/service/SeoulEconomic/Default.aspx"
+HANKOOK_URL = "http://www.paoin.com/Service/Hankooki2018/Default.aspx"        
+MK_URL = "http://epaper.mk.co.kr/PaperList.aspx"
 
 def chosun(): 
     """crawls news headline from chosun, and link from naver, then adds 
@@ -448,6 +450,9 @@ def munhwa():
     return crawl_result_list
 
 
+
+
+
 def kmib():
     """crawls title and link from kmib and naver site, and stores (date, page, title, link) into db"""
 
@@ -555,3 +560,365 @@ def kmib():
     
     crawl_result_list.extend(success_quotes)
     return crawl_result_list
+
+
+
+
+def seoul():
+    """crawls title from seoul economic and link from naver, and stores (date, page, title, link) into db"""
+
+    crawl_result_list = []
+
+    response = requests.get(SEOUL_URL)
+    if response.status_code != 200:
+        crawl_result_list.append(RESPONSE_ERROR_QUOTE)
+        return crawl_result_list  
+    resource = BeautifulSoup(response.text,features = "html.parser")
+    
+    #below code crawls date from seoul economic site
+    selected_elements = resource.find_all(name= "option", attrs={"selected":"selected"})
+    if len(selected_elements) < 3:
+        crawl_result_list.append(DATE_NONE_QUOTE)
+        return crawl_result_list
+    year = int(selected_elements[0].get("value"))
+    month = int(selected_elements[1].get("value"))
+    day = int(selected_elements[2].get("value"))
+    date_in_class_date = date(year, month, day)
+    date_with_dash = str(date_in_class_date)
+    date_today = int(date_with_dash.replace("-",""))
+
+    #below code crawls how many bottom pages are in at seoul economic site
+    a_elements = resource.find_all(name = "a", attrs = {"style" : "padding-top:2px;cursor:hand"})
+    if len(a_elements) == 0:
+        crawl_result_list.append(PAGE_ZERO_QUOTE)
+        return crawl_result_list
+    page_set = set()
+    for a_element in a_elements:
+        href = a_element.get("href")
+        page_number = int(href.split("(")[1].split(")")[0])
+        page_set.add(page_number)
+    page_list = sorted(list(page_set))
+
+    #Now code of extracting titles starts
+    news_list = []
+    numOfWrongMedia = 0
+    numOfAd = 0
+    numOfPage = 1
+    numOfNone = 0
+    numOfHeadline = 0
+
+    for page in page_list:
+        response = requests.post(SEOUL_URL+"?PaperDate="+date_with_dash, data = {"searchpage": page, "searchdate":date_with_dash})
+        if response.status_code != 200:
+            crawl_result_list.append(RESPONSE_ERROR_QUOTE)
+            return crawl_result_list
+
+        resource = BeautifulSoup(response.text,features = "html.parser")
+        paperlist_elements =  resource.find_all(name="div", attrs={"class":"paperlist"})    
+        if len(paperlist_elements) == 0:
+            crawl_result_list.append(PAPERLIST_ZERO_QUOTE)
+            return crawl_result_list
+
+        for paperlist_element in paperlist_elements:
+            a_elements = paperlist_element.find_all(name = "a")
+            for a_element in a_elements:
+                news = {}
+                title = a_element.get_text().strip()
+                news['title'] = title
+                news['page'] = numOfPage
+                news['date'] = date_today
+
+                if "[" in title and "]" in title:
+                    inside_bracket = title.split('[',1)[1].split(']')[0]
+                    if title.split('[')[0] == '' and title.split(']')[1] == '':
+                        numOfAd = numOfAd + 1
+                        break
+                    title = title.replace("["+inside_bracket+"]","")
+
+                title_encoded = urllib.parse.quote(title)
+                url = NAVER_SEARCH_URL + title_encoded
+                response = requests.get(url)
+                resource = BeautifulSoup(response.text,features = "html.parser")
+                a_element = resource.find(name ="a", attrs ={ "class":"_sp_each_title"})
+                if a_element != None:
+                    link =  a_element.get('href')
+                    if link.find("sedaily") == -1:
+                        numOfWrongMedia = numOfWrongMedia + 1
+                        news['link'] = None
+                    else:
+                        news['link'] = link
+                else:
+                    news['link'] = None
+                    numOfNone = numOfNone + 1
+
+                news_list.append(news)
+                numOfHeadline = numOfHeadline + 1
+
+            numOfPage = numOfPage + 1       
+    
+    
+    if numOfHeadline == 0:
+        crawl_result_list.append(HEADLINE_ZERO_QUOTE)
+        return crawl_result_list
+    else:
+        failure_percentage = round((numOfNone + numOfWrongMedia) /numOfHeadline * 100, 2)
+    
+    report = {}
+    report['date'] = date_today
+    report['news'] = 'seoul'
+    report['whole'] = numOfHeadline
+    report['none'] = numOfNone + numOfWrongMedia
+    report['failure'] = failure_percentage
+        
+    success_quotes = []
+    success_quotes.append("number of none: " + str(numOfNone))
+    success_quotes.append("number of msmatched news: " + str(numOfWrongMedia))
+    success_quotes.append("number of ads: " + str(numOfAd))
+    success_quotes.append("number of headline: " + str(numOfHeadline))
+    success_quotes.append("failure percentage: " + str(failure_percentage) + "%")
+    
+    crawl_result_list.extend(success_quotes)
+    return crawl_result_list
+
+
+
+
+
+def hankook():
+    
+    """DEPRECATED"""
+
+    """crawls title from hankook and link from naver, and stores (date, page, title, link) into db"""
+
+    crawl_result_list = []
+
+    response = requests.get(HANKOOK_URL)
+    if response.status_code != 200:
+        crawl_result_list.append(RESPONSE_ERROR_QUOTE)
+        return crawl_result_list  
+    resource = BeautifulSoup(response.text,features = "html.parser")
+    
+    #below code crawls date from seoul economic site
+    selected_elements = resource.find_all(name= "option", attrs={"selected":"selected"})
+    if len(selected_elements) < 4:
+        crawl_result_list.append(DATE_NONE_QUOTE)
+        return crawl_result_list
+    year = int(selected_elements[1].get("value"))
+    month = int(selected_elements[2].get("value"))
+    day = int(selected_elements[3].get("value"))
+    date_in_class_date = date(year, month, day)
+    date_with_dash = str(date_in_class_date)
+    date_today = int(date_with_dash.replace("-",""))
+
+    #below code crawls how many bottom pages are in at seoul economic site
+    a_elements = resource.find_all(name = "a", attrs = {"style" : "padding-top:2px;cursor:hand"})
+    if len(a_elements) == 0:
+        crawl_result_list.append(PAGE_ZERO_QUOTE)
+        return crawl_result_list
+    page_set = set()
+    for a_element in a_elements:
+        href = a_element.get("href")
+        page_number = int(href.split("(")[1].split(")")[0])
+        page_set.add(page_number)
+    page_list = sorted(list(page_set))
+
+    #Now code of extracting titles starts
+    news_list = []
+    numOfWrongMedia = 0
+    numOfAd = 0
+    numOfPage = 1
+    numOfNone = 0
+    numOfHeadline = 0
+
+    for page in page_list:
+        response = requests.post(HANKOOK_URL, data = {"page": page })
+        if response.status_code != 200:
+            crawl_result_list.append(RESPONSE_ERROR_QUOTE)
+            return crawl_result_list
+
+        resource = BeautifulSoup(response.text,features = "html.parser")
+        paperlist_elements =  resource.find_all(name="div", attrs={"class":"paperlist"})    
+        if len(paperlist_elements) == 0:
+            crawl_result_list.append(PAPERLIST_ZERO_QUOTE)
+            return crawl_result_list
+
+        for paperlist_element in paperlist_elements:
+            a_elements = paperlist_element.find_all(name = "a")
+            for a_element in a_elements:
+                news = {}
+                title = a_element.get("title")
+                news['title'] = title
+                news['page'] = numOfPage
+                news['date'] = date_today
+
+                if "[" in title and "]" in title:
+                    inside_bracket = title.split('[',1)[1].split(']')[0]
+                    if title.split('[')[0] == '' and title.split(']')[1] == '':
+                        numOfAd = numOfAd + 1
+                        break
+                    title = title.replace("["+inside_bracket+"]","")
+
+                title_encoded = urllib.parse.quote(title)
+                url = NAVER_SEARCH_URL + title_encoded
+                response = requests.get(url)
+                resource = BeautifulSoup(response.text,features = "html.parser")
+                a_element = resource.find(name ="a", attrs ={ "class":"_sp_each_title"})
+                if a_element != None:
+                    link =  a_element.get('href')
+                    if link.find("hankook") == -1:
+                        numOfWrongMedia = numOfWrongMedia + 1
+                        news['link'] = None
+                    else:
+                        news['link'] = link
+                else:
+                    news['link'] = None
+                    numOfNone = numOfNone + 1
+
+                news_list.append(news)
+                numOfHeadline = numOfHeadline + 1
+
+            numOfPage = numOfPage + 1       
+    
+    
+    if numOfHeadline == 0:
+        crawl_result_list.append(HEADLINE_ZERO_QUOTE)
+        return crawl_result_list
+    else:
+        failure_percentage = round((numOfNone + numOfWrongMedia) /numOfHeadline * 100, 2)
+    
+    report = {}
+    report['date'] = date_today
+    report['news'] = 'hankook'
+    report['whole'] = numOfHeadline
+    report['none'] = numOfNone + numOfWrongMedia
+    report['failure'] = failure_percentage
+        
+    success_quotes = []
+    success_quotes.append("number of none: " + str(numOfNone))
+    success_quotes.append("number of msmatched news: " + str(numOfWrongMedia))
+    success_quotes.append("number of ads: " + str(numOfAd))
+    success_quotes.append("number of headline: " + str(numOfHeadline))
+    success_quotes.append("failure percentage: " + str(failure_percentage) + "%")
+    
+    crawl_result_list.extend(success_quotes)
+    return crawl_result_list
+
+
+def mk():
+    """crawls title from maeil economy and link from naver, and stores (date, page, title, link) into db"""
+
+    crawl_result_list = []
+
+    response = requests.get(MK_URL)
+    if response.status_code != 200:
+        crawl_result_list.append(RESPONSE_ERROR_QUOTE)
+        return crawl_result_list  
+    resource = BeautifulSoup(response.text,features = "html.parser")
+    
+    #below code crawls date from seoul economic site
+    selected_elements = resource.find_all(name= "option", attrs={"selected":"selected"})
+    if len(selected_elements) < 3:
+        crawl_result_list.append(DATE_NONE_QUOTE)
+        return crawl_result_list
+    year = int(selected_elements[0].get("value"))
+    month = int(selected_elements[1].get("value"))
+    day = int(selected_elements[2].get("value"))
+    date_in_class_date = date(year, month, day)
+    date_with_dash = str(date_in_class_date)
+    date_today = int(date_with_dash.replace("-",""))
+
+    #below code crawls how many bottom pages are in at seoul economic site
+    a_elements = resource.find_all(name = "a", attrs = {"style" : "padding-top:2px;cursor:hand"})
+    if len(a_elements) == 0:
+        crawl_result_list.append(PAGE_ZERO_QUOTE)
+        return crawl_result_list
+    page_set = set()
+    for a_element in a_elements:
+        href = a_element.get("href")
+        page_number = int(href.split("(")[1].split(")")[0])
+        page_set.add(page_number)
+    page_list = sorted(list(page_set))
+
+    #Now code of extracting titles starts
+    news_list = []
+    numOfWrongMedia = 0
+    numOfAd = 0
+    numOfPage = 1
+    numOfNone = 0
+    numOfHeadline = 0
+
+    for page in page_list:
+        response = requests.get(MK_URL+"?page=" +str(page))
+        if response.status_code != 200:
+            crawl_result_list.append(RESPONSE_ERROR_QUOTE)
+            return crawl_result_list
+
+        resource = BeautifulSoup(response.text,features = "html.parser")
+        paperlist_elements =  resource.find_all(name="div", attrs={"class":"paperlist"})    
+        if len(paperlist_elements) == 0:
+            crawl_result_list.append(PAPERLIST_ZERO_QUOTE)
+            return crawl_result_list
+
+        for paperlist_element in paperlist_elements:
+            a_elements = paperlist_element.find_all(name = "a")
+            for a_element in a_elements:
+                news = {}
+                title = a_element.get_text().strip()
+                news['title'] = title
+                news['page'] = numOfPage
+                news['date'] = date_today
+
+                if "[" in title and "]" in title:
+                    inside_bracket = title.split('[',1)[1].split(']')[0]
+                    if title.split('[')[0] == '' and title.split(']')[1] == '':
+                        numOfAd = numOfAd + 1
+                        break
+                    title = title.replace("["+inside_bracket+"]","")
+
+                title_encoded = urllib.parse.quote(title)
+                url = NAVER_SEARCH_URL + title_encoded
+                response = requests.get(url)
+                resource = BeautifulSoup(response.text,features = "html.parser")
+                a_element = resource.find(name ="a", attrs ={ "class":"_sp_each_title"})
+                if a_element != None:
+                    link =  a_element.get('href')
+                    if link.find("mk") == -1:
+                        numOfWrongMedia = numOfWrongMedia + 1
+                        news['link'] = None
+                    else:
+                        news['link'] = link
+                else:
+                    news['link'] = None
+                    numOfNone = numOfNone + 1
+
+                news_list.append(news)
+                numOfHeadline = numOfHeadline + 1
+
+            numOfPage = numOfPage + 1       
+    
+    
+    if numOfHeadline == 0:
+        crawl_result_list.append(HEADLINE_ZERO_QUOTE)
+        return crawl_result_list
+    else:
+        failure_percentage = round((numOfNone + numOfWrongMedia) /numOfHeadline * 100, 2)
+    
+    report = {}
+    report['date'] = date_today
+    report['news'] = 'mk'
+    report['whole'] = numOfHeadline
+    report['none'] = numOfNone + numOfWrongMedia
+    report['failure'] = failure_percentage
+        
+    success_quotes = []
+    success_quotes.append("number of none: " + str(numOfNone))
+    success_quotes.append("number of msmatched news: " + str(numOfWrongMedia))
+    success_quotes.append("number of ads: " + str(numOfAd))
+    success_quotes.append("number of headline: " + str(numOfHeadline))
+    success_quotes.append("failure percentage: " + str(failure_percentage) + "%")
+    
+    crawl_result_list.extend(success_quotes)
+    return crawl_result_list
+
+
+
